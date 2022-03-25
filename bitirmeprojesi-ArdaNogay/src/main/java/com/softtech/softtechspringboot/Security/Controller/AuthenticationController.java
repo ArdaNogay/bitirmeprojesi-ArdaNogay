86bb7a0348b41b5a375f9f07284c75b22f606f89
Ceyhan Arda Nogay <ardanogay@hotmail.com>
2022-03-25 04:42:41 +0300

    all steps completed

--- bitirmeprojesi-ArdaNogay/src/main/java/com/softtech/softtechspringboot/Security/Controller/AuthenticationController.java
+++ bitirmeprojesi-ArdaNogay/src/main/java/com/softtech/softtechspringboot/Security/Controller/AuthenticationController.java
@@ -1,10 +1,14 @@
 package com.softtech.softtechspringboot.Security.Controller;
 
+import com.softtech.softtechspringboot.Dto.CategorySaveAndUpdateRequestDto;
 import com.softtech.softtechspringboot.Dto.GeneralResponse;
 import com.softtech.softtechspringboot.Dto.UserSaveAndUpdateRequestDto;
 import com.softtech.softtechspringboot.Security.Dto.LoginRequestDto;
 import com.softtech.softtechspringboot.Security.Service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +23,42 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-    @Operation(tags = "Authentication Controller")
+    @Operation(
+            tags = "Authentication Controller",
+            description = "Allows registered user to login.",
+            summary = "User login",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "AuthenticationService",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = CategorySaveAndUpdateRequestDto.class
+                                    ),
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "Login with registered user example1",
+                                                    summary = "Example registered user 1",
+                                                    description = "Login to the system with registered user",
+                                                    value = "{\n" +
+                                                            "  \"userName\": \"admin\",\n" +
+                                                            "  \"password\": \"softtech\"\n" +
+                                                            "}"
+                                            ),
+                                            @ExampleObject(
+                                                    name = "Login with registered user: example2",
+                                                    summary = "Example registered user 2",
+                                                    description = "Login to the system with registered user",
+                                                    value = "{\n" +
+                                                            "  \"userName\": \"final\",\n" +
+                                                            "  \"password\": \"project\"\n" +
+                                                            "}"
+                                            )
+                                    }
+                            )
+                    }
+            )
+    )
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody LoginRequestDto loginRequestDto){
 
@@ -28,7 +67,46 @@ public class AuthenticationController {
         return ResponseEntity.ok(GeneralResponse.of(token));
     }
 
-    @Operation(tags = "Authentication Controller")
+    @Operation(
+            tags = "Authentication Controller",
+            description = "Creates a new user register.",
+            summary = "User register",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "AuthenticationService",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = CategorySaveAndUpdateRequestDto.class
+                                    ),
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "Create a new user register 1",
+                                                    summary = "Example user register 1",
+                                                    description = "Register to create the user",
+                                                    value = "{\n" +
+                                                            "  \"userName\": \"CeyhanArdaN\",\n" +
+                                                            "  \"password\": \"softtech\",\n" +
+                                                            "  \"name\": \"Ceyhan Arda\",\n" +
+                                                            "  \"surname\": \"Nogay\"\n" +
+                                                            "}"
+                                            ),
+                                            @ExampleObject(
+                                                    name = "Create a new user register 2",
+                                                    summary = "Example user register 2",
+                                                    description = "Register to create the user",
+                                                    value = "{\n" +
+                                                            "  \"userName\": \"ArdaN\",\n" +
+                                                            "  \"password\": \"softtech\",\n" +
+                                                            "  \"name\": \"Ceyhan Arda\",\n" +
+                                                            "  \"surname\": \"Nogay\"\n" +
+                                                            "}"
+                                            )
+                                    }
+                            )
+                    }
+            )
+    )
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody UserSaveAndUpdateRequestDto userSaveAndUpdateRequestDto){
 
--- scripts/import_category.sql
+++ scripts/import_category.sql
@@ -1,12 +1,6 @@
-INSERT INTO category (id, category_type, tax)
-VALUES ('1', 'FOOD', 1);
-INSERT INTO category (id, category_type, tax)
-VALUES ('2', 'STATIONARY', 8);
-INSERT INTO category (id, category_type, tax)
-VALUES ('3', 'CLOTHING', 8);
-INSERT INTO category (id, category_type, tax)
-VALUES ('4', 'TECHNOLOGY', 18);
-INSERT INTO category (id, category_type, tax)
-VALUES ('5', 'CLEANING', 18);
-INSERT INTO category (id, category_type, tax)
-VALUES ('6', 'OTHER', 18);
+INSERT INTO category (id, category_type, tax) VALUES('1', 'FOOD', 1);
+INSERT INTO category (id, category_type, tax) VALUES('2', 'STATIONARY', 8);
+INSERT INTO category (id, category_type, tax) VALUES('3', 'CLOTHING', 8);
+INSERT INTO category (id, category_type, tax) VALUES('4', 'TECHNOLOGY', 18);
+INSERT INTO category (id, category_type, tax) VALUES('5', 'CLEANING', 18);
+INSERT INTO category (id, category_type, tax) VALUES('6', 'OTHER', 18);
